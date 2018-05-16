ddb3d136da2ba39c7e92b90dd4fa8dfa55a8278c
nlazyan <nonalaz@gmail.com>
2018-05-16 09:32:08 +0400

    Add solution for JS Drum Kit(html, css, JS)

---
+++ day1/script.js
@@ -0,0 +1,18 @@
+
+
+function playing(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`div[data-key = "${e.keyCode}"]`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add("playing");
+}
+
+function removeTransition(e) {
+	this.classList.remove("playing");
+}
+
+window.addEventListener("keydown", playing);
+const keys = document.querySelectorAll(`.key`);
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
