5ddbc5d31d62ee7dc105cd909ad8072409ca715b
nlazyan <nonalaz@gmail.com>
2018-05-17 09:37:53 +0400

    Change JS solution after watching day2 video

--- day2/script.js
+++ day2/script.js
@@ -1,31 +1,23 @@
 'use strict'
-let s = new Date().getSeconds()*6+96;
-let m = new Date().getMinutes()*6+96;
-let h = new Date().getHours()*6 +90;
-document.querySelector('.min-hand').style.transform = `rotate(${m}deg)`;;
-document.querySelector('.hour-hand').style.transform = `rotate(${h}deg)`;
 
-console.log(s);
-setInterval(function sec(){
-	let sec = document.querySelector('.sec-hand');
-	sec.style.transform = `rotate(${s}deg)`;
-    s +=6;
-	if(s%360==0){
-		m +=6;
-		setInterval(function min(){
-	      let min = document.querySelector('.min-hand');
-	      min.style.transform = `rotate(${m}deg)`;	      
-        }, 1000);
-    }
-    if(m%360==0&&s%360==0){
-	      h +=6;
-	      setInterval(function hour(){
-	         let hour = document.querySelector('.hour-hand');
-             hour.style.transform = `rotate(${h}deg)`;
-              }, 1000);
-    }
-}, 1000);
+const secondHand = document.querySelector('.sec-hand');
+const minuteHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
 
+function setDate() {
+	const date = new Date();
+	const seconds = date.getSeconds();
+	const secondsDegrees = ((seconds / 60) * 360) + 90;
+	secondHand.style.transform = `rotate( ${secondsDegrees}deg)`;
 
+	const mins = date.getMinutes();
+	const minsDegrees = ((mins / 60) * 360) + 90;
+	minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
+	const hour = date.getHours();
+	const hourDegrees = ((hour / 12) * 360) + 90;
+	hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
+}
+
+setInterval(setDate, 1000);
